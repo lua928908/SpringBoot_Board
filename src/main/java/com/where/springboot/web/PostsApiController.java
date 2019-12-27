2475283ebee008ef1acd7925ae45df15a3c0a9fb
lua928908 <47199153+lua928908@users.noreply.github.com>
2019-12-27 22:43:56 +0900

    등록 API, TEST코드 작성

---
+++ src/main/java/com/where/springboot/web/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.where.springboot.web;
+import com.where.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import service.PostsService;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
---
+++ src/main/java/service/PostsService.java
@@ -0,0 +1,23 @@
+package service;
+
+import com.where.springboot.domain.posts.PostsRepository;
+import com.where.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/*
+    RequiredArgsConstructor가 생성자로 bean객체를 받는 역할을 한다. private가 붙은 필드를 인자값으로 하는 생성자를 롬복이 대신 생성한다.
+    생성자를 직접 안쓰고 롬복 어노테이션을 사용하면 해당 클래스의 의존성 관계가 변경되어도 생성자 코드를 수정하는 일이 발생하지 않는다. (롬복이 알아서 해주니까)
+*/
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
